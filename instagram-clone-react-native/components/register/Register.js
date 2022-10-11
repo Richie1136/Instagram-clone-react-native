bdd7a2b987dd2b3993c25a364f5e1b70d868d003
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-11 13:55:26 -0400

    Created 3 inputs and added a button underneath

--- instagram-clone-react-native/components/register/Register.js
+++ instagram-clone-react-native/components/register/Register.js
@@ -7,9 +7,16 @@ const Register = () => {
   const [password, setPassword] = useState('')
   const [name, setName] = useState('')
 
+  const onSignUp = () => {
+
+  }
+
   return (
     <View>
-      <Text>Register</Text>
+      <TextInput placeholder='name' onChangeText={(name) => setName(name)} />
+      <TextInput placeholder='email' onChangeText={(email) => setEmail(email)} />
+      <TextInput placeholder='password' secureTextEntry={true} onChangeText={(password) => setPassword(password)} />
+      <Button onPress={onSignUp} title="Sign Up" />
     </View>
   )
 }
